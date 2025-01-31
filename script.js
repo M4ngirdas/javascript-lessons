43a2784fe05984b88059a3a3bc394ddd66227904
M4ngirdas <mangirdasrvz@gmail.com>
2025-01-31 17:09:09 +0200

    changes

---
+++ script.js
@@ -0,0 +1,28 @@
+
+let firstName = "Mangirdas"
+let favouriteFood = "chicken"
+let email = "mangirdasmalukas@yahoo.com"
+
+
+console.log(typeof firstName);
+console.log(`Your name is ${firstName}`)
+console.log(`Your favourite food is ${favouriteFood}`)
+console.log(`Your email is ${email}`)
+
+let online = true;
+let forSale = true;
+let isStudent = true
+
+
+
+console.log(`Mangis is online ${online}`)
+console.log(`The car is for sale ${forSale}`)
+console.log(`He is a student ${isStudent}`)
+
+let fullName = "Mangirdas Malukas";
+let age = "15"
+let student = "true"
+
+document.getElementById("p1").textContent = `Your name is ${fullName}`;
+document.getElementById("p2").textContent = `Your age is ${age}`;
+document.getElementById("p3").textContent = `Your are a student: ${student}`;
