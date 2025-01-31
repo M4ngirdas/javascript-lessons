ec79d46a1d12492722614f4427f6733dbd02270e
M4ngirdas <mangirdasrvz@gmail.com>
2025-01-31 18:45:12 +0200

    day one

--- README.md
+++ README.md
@@ -1,11 +1,17 @@
 # 🚀 JavaScript Learning Journey!
+    
+### 📅 I started in 2025-01-31
 
-## 📅 I started in 2025-01-31
+### 🎯My goal: to become decent in JavaScript by making projects and continue to improve my skills
 
 
 <br><br>
+
 ---
 ### Day 1
 
 -  learned some basics
 -  learned variables (boolean and strings)
+- learned arithmetic operators
+    
+---
--- script.js
+++ script.js
@@ -1,28 +1,18 @@
+let students = 30;
 
-let firstName = "Mangirdas"
-let favouriteFood = "chicken"
-let email = "mangirdasmalukas@yahoo.com"
+//students = students + 1;
+//students = students - 1;
+//students = students * 2;
+//students = students / 2;
+//students = students ** 3;
+//let extraStudents = students % 3;
 
+//students += 3;
+//students -= 3;
+//students *= 3;
+//students /= 3;
+//students **= 3
+//students %= 3;
 
-console.log(typeof firstName);
-console.log(`Your name is ${firstName}`)
-console.log(`Your favourite food is ${favouriteFood}`)
-console.log(`Your email is ${email}`)
-
-let online = true;
-let forSale = true;
-let isStudent = true
-
-
-
-console.log(`Mangis is online ${online}`)
-console.log(`The car is for sale ${forSale}`)
-console.log(`He is a student ${isStudent}`)
-
-let fullName = "Mangirdas Malukas";
-let age = "15"
-let student = "true"
-
-document.getElementById("p1").textContent = `Your name is ${fullName}`;
-document.getElementById("p2").textContent = `Your age is ${age}`;
-document.getElementById("p3").textContent = `Your are a student: ${student}`;
+//students++;
+//students--;
