d286682824bebbef40cd796611ce01721335334c
M4ngirdas <mangirdasrvz@gmail.com>
2025-02-19 15:55:55 +0200

    changes

--- lessons/script.js
+++ lessons/script.js
@@ -1,23 +1,18 @@
 
 
-const fullName = "Mangirdas Malukas";
+let loggedIn = false;
+let username;
+let password;
 
-//let firstName = fullName.slice(0, 9);
-//let lastName = fullName.slice(10, 17);
+while(!loggedIn){
+    username = window.prompt("Please enter your username!");
+    password = window.prompt("Please enter your password!");
 
-//let firstChar = fullName.slice(0, 1);
-//let lastChar = fullName.slice(-1);
-
-//let firstName = fullName.slice(0, fullName.indexOf(" "));
-//let lastName = fullName.slice(fullName.indexOf(" ") + 1); 
-
-//console.log(firstName);
-//console.log(lastName);
-
-const email = "example@gmail.com";
-
-let username = email.slice(0, email.indexOf("@"));
-let extension = email.slice(email.indexOf("@") + 1)
-
-console.log(username);
-console.log(extension);
+    if(username === "myUsername" && password === "myPassword"){
+        loggedIn = true;
+        console.log("You are logged in!");
+    }
+    else{
+        console.log("Invalid credentials, please try again!");
+    }
+}
