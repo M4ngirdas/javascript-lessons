09b569fadbdd0afc4741d5f7bfbc8cb49c2f3102
M4ngirdas <mangirdasrvz@gmail.com>
2025-02-23 17:37:31 +0200

    changes

--- lessons/script.js
+++ lessons/script.js
@@ -1,36 +1,10 @@
+// rest parameter collects all arguments passed to a function and makes it into single array
 
 
-const minNum = 1;
-const maxNum = 50;
-const answer = Math.floor(Math.random() * (maxNum - minNum + 1));
-
-let attempts = 0;
-let guess;
-let running = true;
-
-while (running) {
-
-    guess = window.prompt(`Guess a number between ${minNum} - ${maxNum}`)
-    guess = Number(guess)
-    console.log(typeof guess, guess)
-
-    if (isNaN(guess)) {
-        window.alert("Please enter a valid number!")
-    }
-    else if (guess < minNum || guess > maxNum) {
-        window.alert("Please enter a valid number!")
-    }
-    else {
-        attempts++;
-        if (guess < answer) {
-            window.alert("TOO LOW! TRY AGAIN!")
-        }
-        else if (guess > answer) {
-            window.alert("TOO HIGH! TRY AGAIN!")
-        }
-        else {
-            window.alert(`CONGRATULATIONS! You are correct! The answer was ${answer} It took you ${attempts} attempts`)
-            running = false;
-        }
-    }
+function combineStrings(...strings){
+    return strings.join("-")
 }
+
+const fullName = combineStrings("Mr", "Spongebob", "III");
+
+console.log(fullName)
