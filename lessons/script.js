3f36c2114e83fe3048c83a97447d93378cb6d878
M4ngirdas <mangirdasrvz@gmail.com>
2025-02-25 20:17:44 +0200

    changes

--- lessons/script.js
+++ lessons/script.js
@@ -1,10 +1,19 @@
-// rest parameter collects all arguments passed to a function and makes it into single array
 
 
-function combineStrings(...strings){
-    return strings.join("-")
+function rollDice() {
+    const numOfDice = document.getElementById("numOfDice").value;
+    const diceResult = document.getElementById("diceResult");
+    const diceImages = document.getElementById("diceImages");
+    const values = [];
+    const images = [];
+
+    for (let i = 0; i < numOfDice; i++) {
+        const value = Math.floor(Math.random() * 6) + 1;
+        values.push(value);
+        images.push(`<img src="/dice-images/${value}.png" alt="Dice ${value}">`);
+    }
+
+    diceResult.textContent = `dice: ${values.join(", ")}`;
+    diceImages.innerHTML = images.join(" ");
+
 }
-
-const fullName = combineStrings("Mr", "Spongebob", "III");
-
-console.log(fullName)
